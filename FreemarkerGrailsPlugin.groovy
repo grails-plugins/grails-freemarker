662ad3f9d50928e63dc04a194cf4025b3a13652b
brownj <brownj@832c1c66-4827-0410-8465-ccb17913cd09>
2009-07-27 22:42:20 +0000

    update version number, exclude some testing artifacts
    git-svn-id: https://svn.codehaus.org/grails-plugins/grails-freemarker/trunk@54387 832c1c66-4827-0410-8465-ccb17913cd09

--- FreemarkerGrailsPlugin.groovy
+++ FreemarkerGrailsPlugin.groovy
@@ -2,14 +2,16 @@ import grails.util.BuildSettingsHolder
 
 class FreemarkerGrailsPlugin {
     // the plugin version
-    def version = "0.1.1"
+    def version = "0.2-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.1 > *"
     // the other plugins this plugin depends on
     def dependsOn = [:]
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
-            "grails-app/views/error.gsp"
+            "grails-app/views/error.gsp",
+            "grails-app/views/demo/*",
+            "grails-app/controllers/*"
     ]
 
     def author = "Jeff Brown"
