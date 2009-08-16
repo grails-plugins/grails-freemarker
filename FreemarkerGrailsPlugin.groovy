9917105384d7ecb257b2910e44353588f065db5d
brownj <brownj@832c1c66-4827-0410-8465-ccb17913cd09>
2009-08-16 23:27:01 +0000

    update version number
    git-svn-id: https://svn.codehaus.org/grails-plugins/grails-freemarker/trunk@55150 832c1c66-4827-0410-8465-ccb17913cd09

--- FreemarkerGrailsPlugin.groovy
+++ FreemarkerGrailsPlugin.groovy
@@ -2,7 +2,7 @@ import grails.util.BuildSettingsHolder
 
 class FreemarkerGrailsPlugin {
     // the plugin version
-    def version = "0.2"
+    def version = "0.3-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.1 > *"
     // the other plugins this plugin depends on
