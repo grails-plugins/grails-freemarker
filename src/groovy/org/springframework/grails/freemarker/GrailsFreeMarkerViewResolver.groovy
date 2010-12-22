a8f8c74105eeaf3cea806556eb50d6e8fc4e68b1
Jeff Brown <jeff@jeffandbetsy.net>
2010-12-22 10:59:44 -0600

    GRAILSPLUGINS-2708 - if an exception is thrown during loadView, log it

--- src/groovy/org/springframework/grails/freemarker/GrailsFreeMarkerViewResolver.groovy
+++ src/groovy/org/springframework/grails/freemarker/GrailsFreeMarkerViewResolver.groovy
@@ -2,13 +2,16 @@ package org.springframework.grails.freemarker
 
 import org.springframework.web.servlet.View
 import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver
+import org.apache.commons.logging.LogFactory
 
 public class GrailsFreeMarkerViewResolver extends FreeMarkerViewResolver {
+
+    private final exceptionLog = LogFactory.getLog(getClass().getName() + '.EXCEPTION')
 
     public GrailsFreeMarkerViewResolver() { 
         viewClass = GrailsFreeMarkerView
     }
-        
+
     protected View loadView(String viewName, Locale locale) {
         def view = null
         try {
@@ -16,6 +19,7 @@ public class GrailsFreeMarkerViewResolver extends FreeMarkerViewResolver {
         } catch(e) {
             // return null if an exception occurs so the rest of the view
             // resolver chain gets an opportunity to  generate a View
+            exceptionLog.warn 'loadView', e
         }
         return view
     }
