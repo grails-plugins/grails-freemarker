9a0f011f1de634ccba96808ec3373f01c49f0853
Joshua Burnett <joshua@greenbill.com>
2011-07-20 18:46:52 -0500

    moved demo controller to a package

---
+++ grails-app/controllers/testing/DemoController.groovy
@@ -0,0 +1,39 @@
+package testing
+
+class DemoController {
+    
+    def index = {
+        [name: 'Jake', state: 'Missouri']
+    }
+
+	def wtf = {
+		render "wtf"
+	}
+
+    def testExplicitRenderFromController = {
+        render view: 'index', model: [name: 'Abe', state: 'Illinois']
+    }
+
+    def testTaglib = {
+		 //render view: 'testTaglib', model:[firstName: 'Zack', middleName: 'Scott']
+        [firstName: 'Zack', middleName: 'Scott']
+    }
+    
+    def testFlash = {
+        flash.message = 'this message is in flash'
+        redirect action: index
+    }
+
+	def gobaby = {
+		render view: '/gobaby', model: [testvar: 'fly away'], plugin:'freemarker-plugin-test'
+    }
+
+	def bluesky = {
+		render view: 'bluesky', model: [testvar: 'fly away'], plugin:'freemarker-plugin-test'
+    }
+
+	def testGspPlugin = {
+		//control test
+		render template: 'testGspPlugin', model: [name: 'Abe', state: 'Illinois'], plugin:'freemarker-plugin-test'
+    }
+}
