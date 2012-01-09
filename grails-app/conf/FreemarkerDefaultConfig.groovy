aa6201095cfcf1b7a297f19e831ccde9d8c3b7c6
Joshua Burnett <joshua@greenbill.com>
2012-01-08 23:19:51 -0600

    setup for new Geb and conditional versions based on grails version

--- grails-app/conf/FreemarkerDefaultConfig.groovy
+++ grails-app/conf/FreemarkerDefaultConfig.groovy
@@ -10,10 +10,18 @@ grails {
             
             /* A list of template loaders or strings - Strings will be used as beanNames. */
             postTemplateLoaders = null
+
+			//tries to find templates by appending the local to then name
+			localizedLookup = false
+			
+			//when referencing a freemarker view name then require the suffix
+			requireViewSuffix = false
             
             viewResolver { 
-                hideException = false 
+                hideException = true 
             }
+
+			
             
             tags {
                 enabled = true
