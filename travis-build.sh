23c46fac1806450b9df1f5bb2100e85ea098fd79
Alexey Zvegintcev <alexey.zvegintcev@sysgears.com>
2017-04-26 17:33:48 +0300

    Travis config changes

--- travis-build.sh
+++ travis-build.sh
@@ -6,7 +6,7 @@ cd test-projects/free-app && ./gradlew check
 
 if [[ $TRAVIS_BRANCH == 'grails3' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
 	echo "### publishing plugin to bintray"
-	cd ../../freemarker-plugin/ && ./gradlew assemble bintrayUpload
+	cd ../../freemarker-plugin && ./gradlew assemble bintrayUpload
 
 else
   echo "TRAVIS_BRANCH: $TRAVIS_BRANCH"
